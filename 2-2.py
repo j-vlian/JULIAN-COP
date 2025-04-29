1c0fc6360509062243400133ceacaa00f1b40ecd
Julian Ashmore <cptashmore@gmail.com>
2025-04-29 17:16:42 -0400

    This is my code for assignment 2-2

---
+++ 2-2.py
@@ -0,0 +1,9 @@
+# Assign current age and current year
+myCurrentAge = 22
+currentYear = 2025
+
+# Calculate age in 2050
+ageIn2050 = myCurrentAge + (2050 - currentYear)
+
+# Print the result
+print(f"In 2050, you will be {ageIn2050} years old.")

c8a321f3206bf872b9d5a6604c6cd2ddb7017f7b
Julian Ashmore <cptashmore@gmail.com>
2025-04-30 00:53:31 -0400

    3-3

---
+++ Unit 3/3-3 MDY.py
@@ -0,0 +1,39 @@
+# Function:     This program determines if a date entered by the user is valid.  
+# Input:        Interactive
+# Output:       Valid date is printed or user is alerted that an invalid date was entered.
+
+validDate = True
+MIN_YEAR = 0
+MIN_MONTH = 1
+MAX_MONTH = 12
+MIN_DAY = 1
+MAX_DAY = 31
+
+year = int(input("year"))
+month = int(input("month"))
+day = int(input("day"))
+
+# Get the year, then the month, then the day
+# housekeeping()
+
+date=f"{month}/{day}/{year}"
+
+# Check to be sure date is valid
+
+if int(year) <= MIN_YEAR: # invalid year
+    validDate = False
+elif int(month) < MIN_MONTH or int(month) > MAX_MONTH: # invalid month
+    validDate = False
+elif int(day) < MIN_DAY or int(day) > MAX_DAY: # invalid day
+    validDate = False
+
+# Test to see if date is valid and output date and whether it is valid or not
+
+# endOfJob()
+if validDate == True:
+    # Output statement
+    print(date + "is a valid date.")
+    
+else:
+    # Output statement
+    print (date + "is an invalid date.")
